7d5ebb918166788316b6fbe860376857905edcb4
MattJSmith <MattJSmith@users.noreply.github.com>
2016-09-11 18:49:06 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+document.body.style.cursor = 'none';
+
 function draw(){
 	 var ctx = document.getElementById('myCanvas').getContext('2d');
 	var img = new Image();
@@ -5,7 +7,6 @@ function draw(){
 	}, false);
 	img.src = 'ScorchedOwl.png';
 	img.onload = function(){
-		
 		ctx.drawImage(img,0,0, img.width,img.height, 0,0,myCanvas.width, myCanvas.height);
 }
 }
