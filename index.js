3af87d8ae478587f17038af9831c01361830661a
MattJSmith <MattJSmith@users.noreply.github.com>
2016-09-12 21:29:45 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 document.style.cursor = 'none';
 document.onmousemove = = function() {handleMouseMove(event)};
+var ctx;
+var img;
 
 function draw(){
 	var ctx = document.getElementById('myCanvas').getContext('2d');
@@ -11,13 +13,6 @@ function draw(){
 		ctx.drawImage(img,0,0, img.width,img.height, 0,0,myCanvas.width, myCanvas.height);
 }
 }
-function handleMouseMove(e)
-	var ctx = document.getElementById('myCanvas').getContext('2d');
-	var img = new Image();
-	img.addEventListener("load", function() {
-	}, false);
-	img.src = 'ScorchedOwl.png';
-	img.onload = function(){
-		ctx.drawImage(img,e.clientX,e.clientY, img.width,img.height, 0,0,myCanvas.width, myCanvas.height);
-}
+function handleMouseMove(e){
+	ctx.drawImage(img,e.clientX,e.clientY, img.width,img.height, e.clientX,e.clientY,myCanvas.width, myCanvas.height);
 }
