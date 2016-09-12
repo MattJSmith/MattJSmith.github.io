50f954f339e77d2961f05a8735376480317229c1
MattJSmith <MattJSmith@users.noreply.github.com>
2016-09-12 21:20:44 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 document.style.cursor = 'none';
+document.onmousemove = handleMouseMove;
 
 function draw(){
 	var ctx = document.getElementById('myCanvas').getContext('2d');
@@ -10,7 +11,7 @@ function draw(){
 		ctx.drawImage(img,0,0, img.width,img.height, 0,0,myCanvas.width, myCanvas.height);
 }
 }
-function mouse_position(e)
+function handleMouseMove(e)
 	var ctx = document.getElementById('myCanvas').getContext('2d');
 	var img = new Image();
 	img.addEventListener("load", function() {
