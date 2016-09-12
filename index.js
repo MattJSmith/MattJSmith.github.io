11d20fd780fa346dde12a3c07f8ffe9fdc18be88
MattJSmith <MattJSmith@users.noreply.github.com>
2016-09-12 22:46:01 +0100

    Update index.js

--- index.js
+++ index.js
@@ -26,17 +26,7 @@ setInterval(function(){
         {
         	counter = 0;
         }
-}, 300);
-
-/* function draw(){
-	var ctx = document.getElementById('myCanvas').getContext('2d');
-	var img = new Image();
-	img.addEventListener("load", function() {
-	}, false);
-	img.src = 'ScorchedOwl.png';
-	img.onload = function(){
-		ctx.drawImage(img,0,0, img.width,img.height, 0,0,myCanvas.width, myCanvas.height);
-} */
+}, 600);
 
 
 
