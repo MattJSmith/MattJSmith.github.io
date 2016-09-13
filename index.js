e1aa97fdc4b4c9b4457494031be28ca0aaad108b
MattJSmith <MattJSmith@users.noreply.github.com>
2016-09-13 20:57:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
 var img1 = "url('customCursor.png'), auto";
 var img2= "url('customCursor2.png'), auto";
 var img3 = "url('customCursor3.png'), auto";
+var mainContentDiv = document.getElementById('MainContent');
 
-document.body.style.cursor = img1;
+mainContentDiv.style.cursor = img1;
 var counter = 0;
 setInterval(function(){ 
 	if(counter == 0)
         {
-        	document.body.style.cursor = img1;
+        	mainContentDiv.style.cursor = img1;
         	counter = 1;
         }
         else if(counter == 1)
         {
-        	document.body.style.cursor = img2;
+        	mainContentDiv.style.cursor = img2;
         	counter = 2;
         }
         else if(counter == 2)
         {
-        	document.body.style.cursor = img3;
+        	mainContentDiv.style.cursor = img3;
         	counter = 0;
         }
         else
