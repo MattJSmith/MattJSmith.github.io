d2c3e07081f29ebd6f1d5b9221391b54e7a51d26
MattJSmith <matthewjs@outlook.com>
2016-10-03 21:39:34 +0100

    Updated Game
    Refactored game and added random colours and better scoring.

--- canvasDodge.js
+++ canvasDodge.js
@@ -1,36 +1,50 @@
 var canvas = document.getElementById('ballStage');
-var ctx = canvas.getContext('2d');
+var contex = canvas.getContext('2d');
 
-ctx.cur
-var raf;
+var raf; // request animation frame
 var running = false;
 
 var currentScore = 0;
 var highestScore = 0;
 
+var mainText = new textObject("Cursor Dodge!",'green',10,30,20);
+var instructionText = new textObject("Click to begin!",'black',10,80,35);
+var loseText = new textObject("You lost!",'red',250,150,50);
+var scoreText = new textObject("Current score: " + currentScore + "  | Best Attempt: " + highestScore ,'black',260,30,20);
 
-function  getMousePos(canvas, e) {
-  var rect = canvas.getBoundingClientRect(); // abs. size of element
+var ball1 = new newBall(300,150,5,1,randomHexColour());
+var ball2 = new newBall(400,050,-4,1,randomHexColour());
+var ball3 = new newBall(450,250,1,-2,randomHexColour());
 
-  return {
-    x: (e.clientX - rect.left),   // scale mouse coordinates after they have
-    y: (e.clientY - rect.top)     // been adjusted to be relative to element
-  }
-};
+var ballArray = [ball1,ball2,ball3]; //only lines needed to add more balls
 
-function newBall(startX,startY,startVX,startVY){ //turn into an object
+var timedAreaFunction;
+
+mainText.draw();
+scoreText.draw();
+instructionText.draw();
+  
+ ballArray.forEach(function(ballItem){
+	ballItem.draw();
+});
+
+function newBall(startX,startY,startVX,startVY,color){
   this.x = startX;
   this.y = startY;
   this.vx = startVX;
   this.vy = startVY;
   this.radius = 25;
-  this.color = 'red',
+  this.color = color,
   this.draw = function() {
-    ctx.beginPath();
-    ctx.arc(this.x, this.y, this.radius, 0, Math.PI*2, true);
-    ctx.closePath();
-    ctx.fillStyle = this.color;
-    ctx.fill();
+    contex.beginPath();
+    contex.arc(this.x, this.y, this.radius, 0, Math.PI*2, true);
+    contex.closePath();
+    contex.fillStyle = this.color;
+    contex.fill();
+	
+	contex.lineWidth = 1;
+    contex.strokeStyle = 'rgba(5,5,5,0.1)';
+    contex.stroke();
   };
   this.update = function()
   {
@@ -46,77 +60,89 @@ function newBall(startX,startY,startVX,startVY){ //turn into an object
   }
 };
 
-var ball1 = new newBall(300,150,5,1);
-var ball2 = new newBall(400,050,-4,1);
-var ball3 = new newBall(450,250,1,-2);
-
-var ballArray = [ball1,ball2,ball3]; //only lines needed to add more balls
-
-var addBallThing;
+var Ticks_in_HalfSeconds = 0; //1 = 500ms
+function timedArea()
+{
+	Ticks_in_HalfSeconds++;
+	
+	if(running)
+	{
+		currentScore++;
+		scoreText.updateText("Current score: " + currentScore + "  | Best Attempt: " + highestScore );
+		
+		if(Ticks_in_HalfSeconds == 8)
+		{
+			addNewBall();
+			Ticks_in_HalfSeconds = 0;
+		}
+	}
+	else{
+		
+		Ticks_in_HalfSeconds = 0;
+	}
+}
 
 function addNewBall()
 {
 	if(running)
 	{
-		newX = Math.floor(Math.random() * 400) + 50;
-		newY = Math.floor(Math.random() * 250) + 50;
-		newVX = Math.floor(Math.random() * 7) - 4;
-		newVY = Math.floor(Math.random() * 7) - 4;
+		var newX = Math.floor(Math.random() * 400) + 50;
+		var newY = Math.floor(Math.random() * 250) + 50;
+		var newVX = Math.floor(Math.random() * 7) - 4;
+		var newVY = Math.floor(Math.random() * 7) - 4;
 
 		if(newVX == 0){ newVX = 1;}
 		if(newVY == 0){ newVY = 1;}
 
-		var tempBall = new newBall(newX,newY,newVX,newVY);
-
+		var colour = randomHexColour();
+		var tempBall = new newBall(newX,newY,newVX,newVY,colour);
+		
 		ballArray.push(tempBall);	
-
-		currentScore+=10;	
 	}
-
 }
 
-var mainText = {
-  draw: function() {
-    ctx.font="20px Georgia";
-	ctx.fillStyle = 'green';
-	ctx.fillText("Cursor Dodge!",10,30);
-  }
+function textObject(fillText,colour,x,y,textSize)
+{
+	this.viewingText= fillText;
+	
+	this.draw = function() {
+		contex.font= textSize + "px Georgia";
+		contex.fillStyle = colour;
+		contex.fillText(this.viewingText,x,y);	
+	};
+	this.updateText = function(newText)
+	{
+			this.viewingText = newText;
+	}
 };
 
-var instructionText = {
-  draw: function() {
-    ctx.font="20px Georgia";
-	ctx.fillStyle = 'green';
-	ctx.fillText("Click to begin!",10,80);
-  }
-};
-
-var loseText = {
-  draw: function() {
-    ctx.font="20px Georgia";
-	ctx.fillStyle = 'black';
-	ctx.fillText("You lost!",150,50);
-  }
-};
-
-var scoreText = {
-  draw: function() {
-    ctx.font="20px Georgia";
-	ctx.fillStyle = 'black';
-	ctx.fillText("Current score: " + currentScore + "  | Best Attempt: " + highestScore,260,30);
-  }
-};
-
-function clear() {
-  ctx.fillStyle = 'rgba(255, 255, 255, 0.3)';
-  ctx.fillRect(0,0,canvas.width,canvas.height);
-
-
+function randomHexColour()
+{
+	var outcome = "#";
+	var possible = "0123456789ABCDEF";
+	
+	for( var i=0; i < 6; i++ ){	
+		outcome += possible.charAt(Math.floor(Math.random() * possible.length));
+	}
+	
+	return outcome;
+	
 }
 
-function background() {
-  ctx.fillStyle = 'rgba(185, 185, 0, 0.3)';
-  ctx.fillRect(0,0,canvas.width,canvas.height);
+function draw() {
+
+  clear();
+  
+  mainText.draw();
+
+  scoreText.draw();
+  
+  ballArray.forEach(function(ballItem){
+	  ballItem.draw();
+	  ballItem.update();
+  });
+  
+  raf = window.requestAnimationFrame(draw);
 }
 
 function gameOver()
@@ -130,17 +156,25 @@ function gameOver()
 	instructionText.draw();
 	loseText.draw();
 
-	clearInterval(addBallThing);
-
+	whileNotRunning();
+	
 	ballArray = [ball1,ball2,ball3];
 
 	if(currentScore > highestScore){highestScore = currentScore;}
+	
 	currentScore = 0;
 
 	window.cancelAnimationFrame(raf);
+}
 
+function clear() {
+  contex.fillStyle = 'rgba(255, 255, 255, 0.3)';
+  contex.fillRect(0,0,canvas.width,canvas.height);
+}
 
-	
+function background() {
+  contex.fillStyle = 'rgba(185, 185, 0, 0.3)';
+  contex.fillRect(0,0,canvas.width,canvas.height);
 }
 
 function collision(x1,y1,x2,y2,tolerance)
@@ -154,28 +188,18 @@ function collision(x1,y1,x2,y2,tolerance)
 	return false;
   }
 }
-function draw() {
 
-  clear();
-  mainText.draw();
-  scoreText.draw();
-  
-  ballArray.forEach(function(ballItem){
-	  ballItem.draw();
-	  ballItem.update();
-  });
-  
-  raf = window.requestAnimationFrame(draw);
-}
+function  getMousePos(canvas, e) {
+  var rect = canvas.getBoundingClientRect(); // abs. size of element
+
+  return {
+    x: (e.clientX - rect.left),   // scale mouse coordinates after they have
+    y: (e.clientY - rect.top)     // been adjusted to be relative to element
+  }
+};
 
 canvas.addEventListener('mousemove', function(e) {
-  if (!running) {
-    //clear();
-    //ball.x = e.clientX;
-    //ball.y = e.clientY;
-    //ball.draw();
-  }
-  else
+  if (running) 
   {
   var boundry = 20;
   
@@ -183,23 +207,14 @@ canvas.addEventListener('mousemove', function(e) {
 	
 	ballArray.forEach(function(ballItem){
 
-    if(running && collision(ballItem.x,ballItem.y,pos.x,pos.y,20) == true)
+	var tol = 18;
+	var halfTol = tol/2;
+	
+    if(running && collision(ballItem.x + halfTol, ballItem.y + halfTol, pos.x, pos.y, tol) == true)
 	{
 		gameOver();
 	} 
   });
-  
-  
-
-  }
-});
-
-canvas.addEventListener("click", function(e) {
-  if (!running) {
-    raf = window.requestAnimationFrame(draw);
-    running = true;
-    
-    addBallThing = setInterval(addNewBall, 5000);
   }
 });
 
@@ -209,12 +224,23 @@ canvas.addEventListener("mouseout", function(e) {
   
 });
 
-	mainText.draw();
-	scoreText.draw();
-  instructionText.draw();
-  
-  ballArray.forEach(function(ballItem){
-  
-  ballItem.draw();
-  
-  });
+canvas.addEventListener("click", function(e) {
+  if (!running) {
+	  
+    raf = window.requestAnimationFrame(draw);
+    running = true;
+	
+    whileRunning();
+  }
+});
+
+function whileRunning()
+{
+	timedAreaFunction = setInterval(timedArea, 500);
+}
+
+function whileNotRunning()
+{
+	clearInterval(timedAreaFunction);
+}
+
