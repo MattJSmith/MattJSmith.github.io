b2dc25df0cae64ea59fba5b94efe0075eb72b33f
MattJSmith <matthewjs@outlook.com>
2018-06-17 22:57:14 +0100

    update

--- SoloPong.js
+++ SoloPong.js
@@ -35,19 +35,19 @@ var pongTopPaddle;
 var pongLeftPaddle;
 var pongRightPaddle;
 
+var pongGamePaused = false;
 //Have a Choice of how many paddles (one per wall).
 //Draw an outline on walls without paddles to show the ball will bounce off of these
 //Set ammount of paddles/ saftey walls by a button like other game
 
-
 var pongBackgroundColour = 'rgba(150, 175, 175, 1)';
 
 //underlying logic variables
-var pongGameIntervalSpeed = 100; //in milliseconds
+var pongGameIntervalSpeed = 1; //in milliseconds
 
 var pongTicks = 0; //counter based off the pongGameIntervalSpeed
 
-var pongTimedAreaFunction;
+var pongLoopReference;
 
 //Start Game - Actual Code that is running
 
@@ -62,7 +62,10 @@ pongCanvas.addEventListener('mousemove', function(e) {
 });
 
 pongCanvas.addEventListener("mouseout", function(e) {
-		PongGameOver();
+		Pause();
+});
+pongCanvas.addEventListener("mouseover", function(e) {
+		UnPause();
 });
 
 pongCanvas.addEventListener("click", function(e) {
@@ -98,58 +101,6 @@ function PongInitialiseGame()
 	PongPaddleButtonDraw();
 }
 
-function PongCreateBall(startX,startY,startVX,startVY, R,G,B,A){
-	
-  this.x = startX;
-  this.y = startY;
-  this.vx = startVX;
-  this.vy = startVY;
-  this.radius = 25;
-  this.colorR = R,
-  this.colorG = G,
-  this.colorB = B,
-  this.colorA = A,
-  
-  this.draw = function() {
-    pongContex.beginPath();
-    pongContex.arc(this.x, this.y, this.radius, 0, Math.PI*2, true);
-    pongContex.closePath();
-	
-	var color = "rgba(" + this.colorR + ", " + this.colorG + ", " + this.colorB + ", " + this.colorA + ")";
-    pongContex.fillStyle = color;
-    pongContex.fill();
-	
-	pongContex.lineWidth = 1;
-    pongContex.strokeStyle = 'rgba(5,5,5,0.1)';
-    pongContex.stroke();
-  };
-  
-  this.update = function()
-	  {
-		this.x += this.vx;
-		this.y += this.vy;
-		this.vx *= 1.0001;
-			this.vy *= 1.0001;
-		
-		//Do this if collision with paddle
-	
-			
-		if (this.y + this.vy > pongCanvas.height && typeof pongBotPaddle == 'undefined') {
-			this.vy = -this.vy;
-		}
-		else if ( this.y + this.vy < 0 && typeof pongTopPaddle == 'undefined') {
-			this.vy = -this.vy;
-	    }
-	  
-		if (this.x + this.vx > pongCanvas.width && typeof pongRightPaddle == 'undefined') {
-			this.vx = -this.vx;
-		}   
-		else if ( this.x + this.vx < 0 && typeof pongLeftPaddle == 'undefined') {
-			this.vx = -this.vx;
-		} 
-  }
-};
-
 function PongStartButton(e)
 {	
 	if(e.x > PongStartButtonRectangleX && e.x < (PongStartButtonRectangleX + PongStartButtonRectangleWidth) && e.y > PongStartButtonRectangleY && e.y < (PongStartButtonRectangleY + PongStartButtonRectangleHeight) )
@@ -158,8 +109,7 @@ function PongStartButton(e)
 		
 		PongStartGame();
 
-		PongGameBeginDrawing();
-	
+		LoopPongGameDraw();	
 	}
 }
 
@@ -180,34 +130,28 @@ function PongSetPaddleCountButton(e)
 		PongPaddleCountButtonText.draw();
 }
 
-
-
-function PongGameBeginDrawing() {
+function LoopPongGameDraw() {
   
   PongClear();
-  
+	
   pongTopPaddle.draw();
-  pongTopPaddle.update();
-  
+ 
   pongBall.draw();
-  pongBall.update();
   
   pongmainText.draw();
 
   pongscoreText.draw();
   
-  pongraf = window.requestAnimationFrame(PongGameBeginDrawing); //Recursive - This is the drawing loop
- 
+  pongraf = window.requestAnimationFrame(LoopPongGameDraw); //Recursive - This is the drawing loop
 }
 
 function PongStartGame()
 {
-	pongTimedAreaFunction = setInterval(PongGameRunningLoop, pongGameIntervalSpeed); //This loops on interval
+	pongLoopReference = setInterval(LoopPongGameLogic, pongGameIntervalSpeed); //This loops on interval
 	
 	PongSpawnBall();
 	
-	pongTopPaddle = new PongCreatePaddle(10,10, "horizontal");
-	
+	pongTopPaddle = new PongCreatePaddle(10,10, "horizontal");	
 }
 
 function PongStartButtonDraw(){
@@ -224,28 +168,33 @@ function PongPaddleButtonDraw(){
 	pongContex.stroke(); 
 }
 
-function PongGameRunningLoop()
+function LoopPongGameLogic()
 {
-	if(pongrunning)
+	if(pongrunning && !pongGamePaused)
 	{
 		pongTicks++;
-		//PongCollisionWithPaddle();
+		PongCollisionWithPaddle();
 		CheckPongBallStillOnScreen();
 		if(PongCheckMilliSeconds(200))
 		{
 			pongCurrentScore += 1;
 			
-			if(pongCurrentScore > ponghighScore){ponghighScore = pongCurrentScore;}
+			if(pongCurrentScore > ponghighScore){
+				ponghighScore = pongCurrentScore;
+				}
 			
 			pongscoreText.updateText("Current score: " + pongCurrentScore + "  | Best Attempt: " + ponghighScore );
 			pongTicks = 0;
-		}		
+		}
+		pongBall.update();
+		pongTopPaddle.update();		
+		
 	}
 }
 
 function PongCheckMilliSeconds(time) //returns pongTicks based on milliseconds you want
 {
-	var result = (pongTicks % (time / pongGameIntervalSpeed));
+	var result = (pongTicks % (time / 100));
 	
 	if(result == 0){return true;}
 	return false;
@@ -253,12 +202,12 @@ function PongCheckMilliSeconds(time) //returns pongTicks based on milliseconds y
 
 function PongSpawnBall()
 {
-	if(pongrunning )
+	if(pongrunning)
 	{
 		var newX = Math.floor(Math.random() * 300) + 50;
 		var newY = Math.floor(Math.random() * 150) + 50;
-		var newVX = Math.floor(3) ;
-		var newVY = Math.floor(2) ;
+		var newVX = 1.2;
+		var newVY = 0.8;
 
 		if(newVX == 0){ newVX = 1;}
 		if(newVY == 0){ newVY = 1;}
@@ -320,33 +269,77 @@ function PongClear() {
   pongContex.fillStyle = pongBackgroundColour;
   pongContex.fillRect(0,0,pongCanvas.width,pongCanvas.height);
 }
-
-function PongBallCollision(otherX,otherY,ballX,ballY, ballRadius, tolerance)
-{
-	var radius = ballRadius + tolerance; //fairer collision
 	
-  if(otherX <= (ballX + radius) && otherX >= (ballX - radius) && otherY <= (ballY + radius) && otherY >= (ballY - radius))
-  {
+function CheckBallAndTopPaddleCollision(){
+				
+	if( (pongBall.x) < pongTopPaddle.x && (pongBall.x) > pongTopPaddle.width && (pongBall.y) < pongTopPaddle.y && (pongBall.y) > pongTopPaddle.height) return false;
+
 	return true;
-  }
-  else{
-  
-	return false;
-  }
-}
+};
 	
 function PongCollisionWithPaddle()
 {
-	var tol = 0; 
-	
+
 	if(pongrunning)
 	{
-		//if(collide reverse ball velocity)
-		
-		PongGameOver();
-	} 
-		
+		if(CheckBallAndTopPaddleCollision()) {
+			pongBall.changeVX();
+			pongBall.changeVY();
+			}
+
+	} 		
  };
+ 
+ function PongCreateBall(startX,startY,startVX,startVY, R,G,B,A){
+	
+  this.x = startX;
+  this.y = startY;
+  this.vx = startVX;
+  this.vy = startVY;
+  this.radius = 25;
+  this.colorR = R,
+  this.colorG = G,
+  this.colorB = B,
+  this.colorA = A,
+  
+  this.draw = function() {
+    pongContex.beginPath();
+    pongContex.arc(this.x, this.y, this.radius, 0, Math.PI*2, true);
+    pongContex.closePath();
+	
+	var color = "rgba(" + this.colorR + ", " + this.colorG + ", " + this.colorB + ", " + this.colorA + ")";
+    pongContex.fillStyle = color;
+    pongContex.fill();
+	
+	pongContex.lineWidth = 1;
+    pongContex.strokeStyle = 'rgba(5,5,5,0.1)';
+    pongContex.stroke();
+  };
+  
+  this.changeVX = function(){	this.vx = -this.vx;}
+    this.changeVY = function(){	this.vy = -this.vy;}
+  this.update = function()
+	  {
+		this.x += this.vx;
+		this.y += this.vy;
+		this.vx *= 1.0001;
+		this.vy *= 1.0001;
+		
+		if (this.y + this.vy > pongCanvas.height && typeof pongBotPaddle == 'undefined') {
+			this.vy = -this.vy;
+		}
+		else if ( this.y + this.vy < 0 && typeof pongTopPaddle == 'undefined') {
+			this.vy = -this.vy;
+	    }
+	  
+		if (this.x + this.vx > pongCanvas.width && typeof pongRightPaddle == 'undefined') {
+			this.vx = -this.vx;
+		}   
+		else if ( this.x + this.vx < 0 && typeof pongLeftPaddle == 'undefined') {
+			this.vx = -this.vx;
+		} 
+  }
+};
  
 function PongCreatePaddle(startX,startY, orientation){
 	
@@ -394,15 +387,6 @@ function PongCreatePaddle(startX,startY, orientation){
 			   }				
 		}
 		  
-	
-
-		//Do this if collision with paddle
-/*	  if (this.y + this.vy > pongCanvas.height || this.y + this.vy < 0) {
-		this.vy = -this.vy;
-	  }
-	  if (this.x + this.vx > pongCanvas.width || this.x + this.vx < 0) {
-		this.vx = -this.vx;
-	  } */
     }
 };
  
@@ -430,7 +414,13 @@ function CheckPongBallStillOnScreen()
 
 function PongEndGame()
 {
-	clearInterval(pongTimedAreaFunction);
+	clearInterval(pongLoopReference);
 }
 
+function Pause(){
+	pongGamePaused = true;
+};
+function UnPause(){
+	pongGamePaused = false;
+};
 
